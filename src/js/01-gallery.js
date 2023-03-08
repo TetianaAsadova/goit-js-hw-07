bedd476e8283e8151b238fffa70c5d9c93e3af80
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-09 00:01:30 +0200

    sum2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(`galleryItems:`, galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+// const ulRef = document.createElement('ul');
+
+const markup = createMarkup(galleryItems);
+
+// ulRef.innerHTML = markup;
+
+galleryRef.innerHTML = markup;
+
+function createMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div> 
+            `;
+        })
+        .join('');
+}
+
+
